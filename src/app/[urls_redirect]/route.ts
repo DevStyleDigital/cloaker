959e4f12e0fbfdb35db0fe23f291d6d92da5bea5
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2023-12-19 23:56:22 -0300

    first commit

--- src/app/[urls_redirect]/route.ts
+++ src/app/[urls_redirect]/route.ts
@@ -203,5 +203,5 @@ export async function GET(
   }
 
   await insertRequest(false, campaign.blockRedirectUrl);
-  return Response.redirect(campaign.blockRedirectUrl, 302)
+  return Response.redirect(campaign.blockRedirectUrl, 302);
 }
